71cb5f724a95473a3ca3c0f4a16a24d7e2b50b50
pollock94 <palok94@gmail.com>
2022-10-18 11:22:16 +0200

    Updated xlm_constants with new levers

--- dmdu/general/xlm_constants_epsilons.py
+++ dmdu/general/xlm_constants_epsilons.py
@@ -581,9 +581,54 @@ def get_lever_names():
 
     lever_names = [
         "sr",
-        "miu",
+        #"miu",
         "irstp_consumption",
-        "irstp_damage"
+        "irstp_damage",
+        "c1",
+        "c2", 
+        "c3",
+
+        "r1",
+        "r2",
+        "r3",
+
+
+        "w0", 
+        "w1", 
+        "w2", 
+        "w3",
+        "w4", 
+        "w5", 
+        "w6", 
+        "w7", 
+        "w8", 
+        "w9", 
+        "w10", 
+        "w11", 
+        "w12", 
+        "w13", 
+        "w14", 
+        "w15", 
+        "w16", 
+        "w17", 
+        "w18", 
+        "w19", 
+        "w20", 
+        "w21", 
+        "w22", 
+        "w23", 
+        "w24", 
+        "w25", 
+        "w26", 
+        "w27", 
+        "w28", 
+        "w29", 
+        "w30", 
+        "w31", 
+        "w32", 
+        "w33", 
+        "w34", 
+        "w35"
     ]
 
     return lever_names
